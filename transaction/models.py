b582cc5afb78cdfccc52c78ce081c457e880cf7a
princewilljackson <mailprincewilljackson@gmail.com>
2023-01-11 13:17:46 +0000

    transaction table updated

--- transaction/models.py
+++ transaction/models.py
@@ -32,7 +32,7 @@ class LocalTransfer(models.Model):
     """User Transfer table"""
     to_fullname = models.CharField(max_length=200)
     bank_name = models.CharField(max_length=100)
-    to_account = models.BigIntegerField()
+    to_account = models.CharField(max_length=150)
     transfer_amount = models.DecimalField(default=0, max_digits=12, decimal_places=2)
     transaction_pin = models.IntegerField(validators=[MinLengthValidator(4)])
     transfer_description = models.CharField(max_length=100, null=True, blank=True)
